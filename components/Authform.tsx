455bb1c75ef05561db5ba77242b60b0b8a14767c
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-06-19 20:01:55 +0530

    added authform and built a component

--- components/Authform.tsx
+++ components/Authform.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import React from 'react'
+import React from 'react';
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z,ZodType } from 'zod';
-// interface Props<T extends >{
- 
-// }
-import { Button } from "@/components/ui/button"
+import {
+  useForm,
+  SubmitHandler,
+  FieldValues,
+  UseFormReturn,
+  DefaultValues,
+} from "react-hook-form";
+import { z, ZodType } from 'zod';
+
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -16,83 +20,86 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import Link from 'next/link';
+import { FIELD_NAMES } from '@/constants';
+import ImageUpload from './ImageUpload';
 
-interface Props<T extends FieldValues > {
-   schema:ZodType<T>;
-   defaultValues:T;
-   onSubmit:(data:T)=>Promise<{success:boolean,error?:string}>
-   type:"SIGN_IN"|"SIGN_UP"; 
+interface Props<T extends FieldValues> {
+  schema: ZodType<T>;
+  defaultValues: DefaultValues<T>;
+  onSubmit: (data: T) => Promise<{ success: boolean; error?: string }>;
+  type: "SIGN_IN" | "SIGN_UP";
 }
 
-const AuthForm = <T extends FieldValues> ({
-    type,
-    schema,
-    defaultValues,
-    onSubmit}:
-    Props<T>) => {
-const isSignIn=type==="SIGN_IN";
- const form:UseFormReturn<T> = useForm<z.infer<typeof formSchema>>({
+const AuthForm = <T extends FieldValues>({
+  schema,
+  defaultValues,
+  onSubmit,
+  type,
+}: Props<T>) => {
+  const isSignIn = type === "SIGN_IN";
+
+  const form = useForm<T>({
     resolver: zodResolver(schema),
-    defaultValues:defaultValues as DefaultValues<T>, 
-    
-    
+    defaultValues,
   });
- 
-  
- const handleSubmit:SubmitHandler<T> async(data) =>{};
+
+  const handleSubmit: SubmitHandler<T> = async (data) => {
+    await onSubmit(data);
+  };
 
   return (
     <div className="flex flex-col gap-4">
-        <h1 className='text-2xl font-semibold text-white'>
-            {isSignIn ?'Welcome Back to BookApp':"Create your library account"}
-            </h1>
-            <p className=''text-light-100>
-               {isSignIn ? 'Sign in to continue ':'Sign up to get started '}
-            </p>
-<Form {...form}>
-      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6 w-full">
+      <h1 className="text-2xl font-semibold text-white">
+        {isSignIn ? 'Welcome Back to BookApp' : 'Create your library account'}
+      </h1>
+      <p className="text-light-100">
+        {isSignIn ? 'Sign in to continue' : 'Sign up to get started'}
+      </p>
 
-{Object.keys(defaultValues).map((field)=>(
-<FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-)}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6 w-full">
+          {Object.keys(defaultValues).map((field) => (
+            <FormField
+              key={field}
+              control={form.control}
+              name={field as  Path<T>}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className='capitalize'>{FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}</FormLabel>
+                  <FormControl>
+                    {field.name==="universityCard" ?(
+                        <ImageUpload/>
+                    ):(
+                        <Input required type={
+                            {FIELD_TYPES[field.name as keyof typeof FIELD_TYPES]}
+                        } {...field}
+                        />
+                    )}
+                    <Input placeholder={field.name} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
 
-        
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-    <p className='text-center text-base font-medium'>
-        {isSignIn ? "New to Bookwise" : "Already have an account"}
-
-        <Link href={isSignIn ?'/sign-up':'/sign-in'} clasName="font-bold text-primary ">
-        {isSignIn ? "Create an account": "Sign in"}
+      <p className="text-center text-base font-medium text-white">
+        {isSignIn ? "New to Bookwise?" : "Already have an account?"}{" "}
+        <Link
+          href={isSignIn ? "/sign-up" : "/sign-in"}
+          className="font-bold text-primary"
+        >
+          {isSignIn ? "Create an account" : "Sign in"}
         </Link>
-    </p>
-        </h1>
- 
+      </p>
     </div>
-   
-  )
-   
-  
+  );
 };
 
-
-export default AuthForm
+export default AuthForm;
